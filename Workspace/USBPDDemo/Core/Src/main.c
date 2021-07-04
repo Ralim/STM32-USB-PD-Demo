1d206b6dc5803256838c59318d51750328ba2eda
Ben V. Brown <Ralim@Ralimtek.com>
2021-07-04 20:57:20 +1000

    Import pd lib

--- Workspace/USBPDDemo/Core/Src/main.c
+++ Workspace/USBPDDemo/Core/Src/main.c
@@ -26,7 +26,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "PDUser.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -92,7 +92,7 @@ int main(void)
   MX_I2C1_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  pd_user_main();
   /* USER CODE END 2 */
 
   /* Infinite loop */
---
+++ Workspace/USBPDDemo/Core/user/PDUser.h
@@ -0,0 +1,21 @@
+/*
+ * PDUser.h
+ *
+ *  Created on: 4 Jul 2021
+ *      Author: ralim
+ */
+
+#ifndef USER_PDUSER_H_
+#define USER_PDUSER_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f4xx_hal.h"
+#include "i2c.h"
+void pd_user_main();
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USER_PDUSER_H_ */
